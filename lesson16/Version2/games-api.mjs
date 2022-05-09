8838127ef45f810a3231f360dbbb6f1b516ae332
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2022-05-09 20:36:51 +0100

    Lesson17: Games API, implementes with server, api and services modules.

--- lesson16/Version2/games-api.mjs
+++ lesson16/Version2/games-api.mjs
@@ -1,42 +1,45 @@
-// File rsponsibilities
+// File responsibilities
+// Register game api routes
 // Have the functions that handle HTTP requests
 
-export default {
-    getGames: getGames,
-    getGame: getGame,
-    createGame: createGame,
-    deleteGame: deleteGame,
-    updateGame: updateGame,
+import express from 'express'
+
+// export default function (app) {
+//     if(!app)
+//         throw "Only work with Beer!!!"
+
+const app = express.Router()
+export default app
+
+app.get('/api/games', getGames)           // Get all jokes
+app.get('/api/games/:id', getGame)        // Get a joke details
+app.delete('/api/games/:id', deleteGame)  // Delete a joke
+app.put('/api/games/:id', updateGame)     // Update a joke
+app.post('/api/games', createGame)        // Delete a joke
+
+function getGames(req, resp) {
+    resp.json({ message: "Get Games" })
 }
 
-
-function getGames(req, resp){
-    resp.json({message : "Get Games" })
+function getGame(req, resp) {
+    resp.json({ message: "getGameByID id = " + req.params.id })
 }
 
-function getGame(req, resp){
-    resp.json({message : "getGameByID id = " + req.params.id })
-}
-
-function updateGame(req, resp){
+function updateGame(req, resp) {
     console.log('Received in body: ', req.body)
-    resp.json({message : "updateGame id = " + req.params.id })
-    
+    resp.json({ message: "updateGame id = " + req.params.id })
+
 }
 
-function createGame(req, resp){
+function createGame(req, resp) {
     console.log('Received in body: ', req.body)
     resp
         .status(201)
-        .json({message : "createGame" })
+        .json({ message: "createGame" })
 }
 
-function deleteGame(req, resp){ 
-    resp.json({message : "deleteGame id = " + req.params.id })
+function deleteGame(req, resp) {
+    resp.json({ message: "deleteGame id = " + req.params.id })
 }
-
-
-
-
 
 
